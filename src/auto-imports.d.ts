2e65e2bd72746923bc02fdc3c36a2c1d7b14f31d
xiaoforest <gaojinshan83@gmail.com>
2023-02-21 16:06:49 +0800

    cs

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -50,6 +50,7 @@ declare global {
   const useAttrs: typeof import('vue')['useAttrs']
   const useCssModule: typeof import('vue')['useCssModule']
   const useCssVars: typeof import('vue')['useCssVars']
+  const useEventListener: typeof import('vue-hooks-plus')['useEventListener']
   const useLink: typeof import('vue-router')['useLink']
   const useRoute: typeof import('vue-router')['useRoute']
   const useRouter: typeof import('vue-router')['useRouter']
--- src/views/components/tabPublicList.vue
+++ src/views/components/tabPublicList.vue
@@ -12,47 +12,78 @@
       </div>
     </div>
 
-    <ul class="public-list title">
-      <li>
-        <div class="num">序号</div>
-        <div class="text">标题</div>
-        <div class="time">发布时间</div>
-        <div class="people">已读人数</div>
-      </li>
-    </ul>
+    <div class="tableTitle change">
+      <table class="tableStyle one">
+        <thead>
+          <tr>
+            <th :width="item.width" v-for="(item, index) in TheTabs">
+              {{ item.name }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ TheList[0].id }}</td>
+            <td>{{ TheList[0].text }}</td>
+            <td>{{ TheList[0].time }}</td>
+            <td>{{ TheList[0].value }}人</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="tableScroll cp" style="height: 400px">
+      <table class="tableStyle two">
+        <thead>
+          <tr>
+            <th :width="item.width" v-for="(item, index) in TheTabs">
+              {{ item.name }}
+            </th>
+          </tr>
+        </thead>
+        <tbody v-show="tabNumber == 0">
+          <tr v-for="(item, index) in TheList[0].list" @click="onTcMod(item)">
+            <td>{{ index + 1 }}</td>
+            <td>{{ item.text }}</td>
+            <td>{{ item.time }}</td>
+            <td>{{ item.value }}人</td>
+          </tr>
+        </tbody>
+        <tbody v-show="tabNumber == 1">
+          <tr v-for="(item, index) in TheList[1].list" @click="onTcMod(item)">
+            <td>{{ index + 1 }}</td>
+            <td>{{ item.text }}</td>
+            <td>{{ item.time }}</td>
+            <td>{{ item.value }}人</td>
+          </tr>
+        </tbody>
+        <tbody v-show="tabNumber == 2">
+          <tr v-for="(item, index) in TheList[2].list" @click="onTcMod(item)">
+            <td>{{ index + 1 }}</td>
+            <td>{{ item.text }}</td>
+            <td>{{ item.time }}</td>
+            <td>{{ item.value }}人</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
-    <ul class="public-list list" v-show="tabNumber == 0">
-      <li v-for="(item, index) in TheList[0].list" :key="index">
-        <div class="num">{{ index + 1 }}</div>
-        <div class="text">{{ item.text }}</div>
-        <div class="text">{{ item.time }}</div>
-        <div class="people">{{ item.value }}人</div>
-      </li>
-    </ul>
-
-    <ul class="public-list list" v-show="tabNumber == 1">
-      <li v-for="(item, index) in TheList[1].list" :key="index">
-        <div class="num">{{ index + 1 }}</div>
-        <div class="text">{{ item.text }}</div>
-        <div class="text">{{ item.time }}</div>
-        <div class="people">{{ item.value }}人</div>
-      </li>
-    </ul>
-
-    <ul class="public-list list" v-show="tabNumber == 2">
-      <li v-for="(item, index) in TheList[2].list" :key="index">
-        <div class="num">{{ index + 1 }}</div>
-        <div class="text">{{ item.text }}</div>
-        <div class="text">{{ item.time }}</div>
-        <div class="people">{{ item.value }}人</div>
-      </li>
-    </ul>
+  <div class="tc_mod" :class="{ show: popupShow == true }">
+    <div class="bg" ref="divRef"></div>
+    <div class="tc-information">
+      <div class="title">{{ tcTitle }}</div>
+      <div class="text" v-html="tcText"></div>
+    </div>
+    <div class="icon-close" @click="popupShow = false">
+      <svg class="icon-svg"><use xlink:href="#svg-close"></use></svg>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Strong } from "mdast"
 import { defineComponent, onMounted, inject, computed, ref } from "vue"
+import { useEventListener } from "vue-hooks-plus"
 const props = defineProps({
   TheList: {
     type: Object,
@@ -60,13 +91,55 @@ const props = defineProps({
   }
 })
 let tabNum = ref<Number>(0)
+let tabMainId = ref<Number>(0)
 const tabNumber = computed<any>(() => {
   return tabNum.value
 })
-const onTabFun: any = (i: Number) => {
+const onTabFun: any = (i: Number, index: Number) => {
   tabNum.value = i
+  tabMainId.value = index
 }
+
+let TheTabs = ref([
+  {
+    name: "序号",
+    width: "12%"
+  },
+  {
+    name: "标题",
+    width: "35%"
+  },
+  {
+    name: "发布时间",
+    width: "25%"
+  },
+  {
+    name: "已读人数",
+    width: "25%"
+  }
+])
+
 onMounted(() => {})
+
+let popupShow = ref<boolean>(false)
+let tcTitle = ref<String>("")
+let tcText = ref<String>("")
+
+const divRef = ref()
+useEventListener(
+  "click",
+  () => {
+    popupShow.value = false
+  },
+  { target: divRef }
+)
+
+const onTcMod = (i: Object, index: Number) => {
+  console.log(i)
+  popupShow.value = true
+  tcTitle.value = i.title
+  tcText.value = i.details
+}
 </script>
 
 <style scoped lang="less">
